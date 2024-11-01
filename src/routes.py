2a068aca2530e5e8e337052c862e47904dd9bfab
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-11-01 23:50:46 +0200

    Add authentication routes

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,42 @@
 from app import app
-from flask import render_template
+from flask import redirect, render_template, request, session
 
-@app.route("/")
+import users
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        print(request.get_data())
+
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.login(username, password):
+            return redirect("/")
+        return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.register(username, password):
+            return redirect("/")
+        return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
