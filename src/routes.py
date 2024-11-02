0c19d79ac15490308b99fccde31950c4a41d4dc4
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-11-02 03:31:57 +0200

    Add editcompany route

--- src/routes.py
+++ src/routes.py
@@ -1,13 +1,13 @@
+from pydantic import ValidationError
 from app import app
 from flask import redirect, render_template, request, session
 
 import users
+import companies
+from validators import CompanyForm, LoginForm, RegisterForm, formatErrors
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        print(request.get_data())
-
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
@@ -16,12 +16,14 @@ def login():
         return render_template("login.html")
 
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        if users.login(username, password):
+        try:
+            formData = LoginForm(**request.form.to_dict())
+        except ValidationError as e:
+            return render_template("login.html", errorMessage=formatErrors(e.errors()))
+        
+        if users.login(formData.username, formData.password):
             return redirect("/")
-        return render_template("error.html", message="Väärä tunnus tai salasana")
+        return render_template("login.html", errorMessage="Väärä tunnus tai salasana")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -29,14 +31,32 @@ def register():
         return render_template("register.html")
 
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        try:
+            formData = RegisterForm(**request.form.to_dict())
+        except ValidationError as e:
+            return render_template("register.html", message=formatErrors(e.errors()))
 
-        if users.register(username, password):
+        if users.register(formData.username, formData.password):
             return redirect("/")
-        return render_template("error.html", message="Väärä tunnus tai salasana")
+        return render_template("error.html", "Rekisteröinnissä tapahtui virhe")
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
+
+@app.route("/editcompany", methods=["GET", "POST"])
+@app.route("/editcompany/<int:id>", methods=["GET", "POST"])
+def editcompany(id=None):
+    if request.method == "GET":
+        return render_template("editcompany.html", id=id)
+
+    if request.method == "POST":
+        try:
+            formData = CompanyForm(**request.form.to_dict())
+        except ValidationError as e:
+            return render_template("editcompany.html", errorMessage=formatErrors(e.errors()))
+
+        if companies.upsertCompany(formData, id):
+            return redirect("/")
+        return render_template("error.html", message="Yrityksen luomisessa tapahtui virhe")
