cb313c069cf256cf7b875077a864475e0bf13ab9
Markus Wahlman <wahlman2002@gmail.com>
2025-10-18 17:09:22 +0300

    fix linting

--- src/jinjaUtils.py
+++ src/jinjaUtils.py
@@ -5,6 +5,7 @@ Jinja utility processor for adding utility functions to Flask templates.
 from flask import request, url_for, session
 from services.userServices import UserRoles
 
+
 def utilityProcessor():
     """
     Utility functions for the Jinja context.
@@ -16,14 +17,14 @@ def utilityProcessor():
     def hasEditAccess():
         role = session.get('role')
         if not role or UserRoles(role) is not UserRoles.OWNER and UserRoles(
-                    role) is not UserRoles.ADMIN:
-                return False
+                role) is not UserRoles.ADMIN:
+            return False
         return True
 
     def hasOwnerAccess():
         role = session.get('role')
         if not role or UserRoles(role) is not UserRoles.OWNER:
-                return False
+            return False
         return True
 
     return {
@@ -31,5 +32,3 @@ def utilityProcessor():
         "hasEditAccess": hasEditAccess,
         "hasOwnerAccess": hasOwnerAccess
     }
-
-        
